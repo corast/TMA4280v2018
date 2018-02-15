ca6d46802acd66b40e2f8d0d69415beae49406eb
corast <corastweb94@hotmail.com>
2018-02-15 21:11:20 +0100

    Fixing zet0.cpp and compile issues
    changing include libraries

--- P1/question1/zeta0/main.cpp
+++ P1/question1/zeta0/main.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
-#include "zeta0.cpp"
-using namespace std;
+#include <cstdio> //printf function
+#include <string> // stoi function (string to int)
+
+#include "zeta0.cpp" 
+
+//using namespace std; //to not have to type std:: for every command
+
+
 int main(int argc, char* argv[])
 {
     //check if we got one argument
-    if(argc < 1){
-        printf("Please type one number n");
+    if(argc != 2){
+        std::printf("Please type one number n as argument to this program\n");
         return 1;
     }
-    /*else if(argc >= 2){
-        printf("Please type only one number n, %f", argc);
-        return 2;
-    } */
 
-    int n = strtol(argv[1],NULL, 0);
+    int n = std::stoi(argv[1],NULL,0);
 
-    printf("n set as %i\n",n);
     double pi = zeta0(n);
-    
-    printf("Pi is aproximatly %f with %i itterations", pi, n);
+    printf("Pi is aproximatly %f with n as %i \n", pi, n);
 }
