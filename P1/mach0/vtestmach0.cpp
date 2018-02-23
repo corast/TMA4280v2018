0cab8fe9c77b9e224eb5260384298fab974a5e91
corast <corastweb94@hotmail.com>
2018-02-23 16:06:00 +0100

    question 3
    Validation test question 3

--- P1/mach0/vtestmach0.cpp
+++ P1/mach0/vtestmach0.cpp
@@ -1,8 +1,9 @@
 #include <cmath> //for pow function
-#include "zeta0.h" //for zeta function
+#include "mach0.h" //for zeta function
 //#include <cassert>
 #include <string> //for printf
 #include <fstream> //for file handing: creation, opening, writing etc.
+#include <iomanip>
 
 int validTest();
 //double can relibly hold 15 decimal places.
@@ -23,16 +24,16 @@ int validTest(){
     double pi;
     double error_old;
     double error;
+
     //calculate error for each k, and vertify that it converges to 0 towards k=24.
     for(int k = 1; k<=24;k++){
-        n = pow(2,k);
-        pi = zeta(n);
+        n = 1 << k; //binary bitshit to the right, 2 bits.
+        pi = mach(n);
         error = std::abs(PI-pi);
         std::string line;
-        line = "For n = " + std::to_string(n) + "\t\tError: " + std::to_string(error) + "\n";
-
-        myfile.width(20);
-        myfile << line;
+        myfile << std::setprecision(10) << "for n = "<< n << "\t\tError: " << error << std::endl;
+        //line = "For n = " +std::to_string(n) + "\t\tError: " + std::to_string(error) + "\n";
+        //myfile << line;
         if(error > error_old && k != 1){
             //means we are not convering to a better value than before, we can stop test
             printf("not convering to pi!\n");
--- P1/zeta0/zeta0.cpp
+++ P1/zeta0/zeta0.cpp
@@ -6,7 +6,7 @@ double zeta(int n)
     double S = 0.0;
 
     for(int i = 1; i <= n; i++){
-        S += (double)1/(i*i);
+        S += (double)1/pow(i,2);
     }
     //Now that we have calculated S, we need to multiply by 6, and find the square root.
     //S_n = pi^2/6; 
