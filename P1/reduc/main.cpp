7735c5896453cf1d5ffbc17a231b5dca032f7586
corast <corastweb94@hotmail.com>
2018-03-12 15:00:19 +0100

    Recursive doubling zeta

--- P1/reduc/main.cpp
+++ P1/reduc/main.cpp
@@ -11,7 +11,7 @@
 void mpi_zeta();
 void mpi_mach();
 std::tuple<int, int> calculate_work();
-void recursive_doubling(int *,int *);
+void recursive_doubling(double *,double *);
 
 //Global variables
 int numprocs, n, method, type;
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
         }break;
 
         default: {
-            std::count << "Error, no  more methods to choice from" << std::endl;
+            std::cout << "Error, no  more methods to choice from" << std::endl;
         }
     }
 
@@ -108,7 +108,7 @@ void mpi_zeta(){
     if(!type){//If we want all_reduce
         MPI_Allreduce(&sum, &sum_all, 1 , MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD); //we sum the sum variable from each process and store in pi.
     }else{
-        recursive_doubling(&sum,&sum_all); //Where we want the final sum to be stored.
+        recursive_doubling(&sum, &sum_all); //Where we want the final sum to be stored.
     }
   
 
@@ -163,6 +163,7 @@ bool turnToSend(int distance, int x){//for ALL-TO-ONE sum computing (Recursive h
        x = 2      4, 12, 20, ... (2*2*2*i + 4)
        ect
     */ 
+    printf("Not used");
     int proc = 0;
     for(int i = 0; i<numprocs; i++){ 
         proc = (pow(2,x+1)*i)+distance; //Sequence is (2^x)*i+distance
@@ -182,24 +183,20 @@ bool forwarSendDirection(int i){
     */
     //We need to create an mask corresponding to itteration, to check a given bit.
     int mask = 1;
-    if(i != 0){
-        mask << i;//we need to shift our mask by i bits. mask*2^î 
+    if(i > 0){
+        mask = mask << i;//we need to shift our mask by i bits. mask*2^î 
     }
-    
     int sendDir = mask ^ myid; //xor the mask with myid.
-
     //if the bit position corresponding to the mask is set, we need to send the sum a given distance.
-    std::bitset<sizeof(int)> bit (sendDir);
+    std::bitset<8> bit (sendDir); //8 bits should be enough.
     //if bit nr i has value 1 we should send forward.
     if(bit.test(i)){
-        return true;    
+        return true;     
     }//else we need to send backwards.
     return false;
 }
 
-void recursive_doubling(int *psum, int *recvbuf){
-    MPI_Status status;
-
+void recursive_doubling(double *psum, double *recvbuf){
     //Recursive doubling: ech process pass on the sum and calculate a partial sum from this. First all processes send their sum once, 
     /*
         IF we have 8 processes. Everyone process hold a partial sum and pass on the value. 
@@ -210,11 +207,18 @@ void recursive_doubling(int *psum, int *recvbuf){
         #Ather this exchante og sums, every process should process should be able to compute pi.
         MPI_Sendrecv(sendbuffer, sendcount, Datatype, sendtype, destination, sendtag, receivbuffer, recievcount, Datatype, source , recvtag, comm );
     */
+    MPI_Status status;
 
+    if(myid == 0){
+        printf("My partial sum before is: %f\n",*psum);
+    }    
+    double recdat = 0.0;
     for(int i = 0; i < log(numprocs); i++){
 
         int distance = (int)pow(2,i); //This is the distance between who is sending and who is receiving.
-
+        if(myid == 0){
+            printf("Distance %d at i = %d\n",distance,i);
+        } 
         int sendId = myid;
 
         if(forwarSendDirection(i)){
@@ -223,16 +227,27 @@ void recursive_doubling(int *psum, int *recvbuf){
             sendId -= distance; //send diastance backward
         }
          //printf("i %d, process %d to send to %d, distance %d \n", i, myid, myid-distance, distance);
-        if(method){//if we come from zeta
-            MPI_Sendrecv(&psum, 1, MPI_INT, 0, sendId, &recvbuf, 1, MPI_INT, myid, 0, MPI_COMM_WORLD, &status);
+        if(method == 0){//if we come from zeta
+
+            printf("process %d to send to %d at i=%d\n",myid, sendId, i);
+            MPI_Sendrecv(&psum, 1, MPI_DOUBLE, sendId, 0, &recdat, 1, MPI_DOUBLE, sendId, 0, MPI_COMM_WORLD, &status);
             //After we have sent and recieved we add to the sum
-            *psum += *recvbuf;
+            double sum = recdat; 
+            printf("We received %f\n", sum);
+
+            *psum += recdat;
         }
         else{//mach
+            //Only difference is that we now get two numbers instead for the two arcs
             //MPI_Sendrecv(&psum,1, )
+            //MPI_Sendrecv(&psum, 2, MPI_INT, 0, sendId, &recvbuf, 2, MPI_INT, myid, 0, MPI_COMM_WORLD, &status);
         }
         
            
+    }
+
+    if(myid == 0){
+        printf("My partial sum is: %f\n",*psum);
     }
     //We need to write the sum
     //How does this work?
