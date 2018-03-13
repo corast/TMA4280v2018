3dc8c53df1d7d8f05da24b3261bd0efe46560637
corast <corastweb94@hotmail.com>
2018-03-13 12:22:57 +0100

    Testing different stuffs omp

--- P1/zeta2/zeta2.cpp
+++ P1/zeta2/zeta2.cpp
@@ -1,20 +1,24 @@
 #include "zeta2.h"
 #include <math.h> //for pow function.
 #include <cstdio>
-#include <omp.h>
+#include "omp.h"
 
 double zeta(int n, int threads){ 
     double S = 0.0;
     int tid;
-    omp_set_dynamic(0);
-    omp_set_num_threads(2);
+
+    //omp_set_dynamic(0);
+    //omp_set_num_threads(8);
     printf(" max threads %d\n",omp_get_max_threads());
-    #pragma omp paralell for reduction(+:S) schedule(static)
+    #pragma omp paralell for num_threads(8) schedule(static)
+    {
     for(int i = 1; i <= n; i++){//each process will do an equal amout of itterations.
         tid = omp_get_thread_num();
         int num = omp_get_num_threads();
         S +=1.0 /pow(i,2);
         printf(" thread %d of %d working on %d \n",tid+1, num, i);
     }
+    }
+
     return S;
 }
