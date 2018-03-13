172b8120715629f058533c2c48e8507bc1ff639a
corast <corastweb94@hotmail.com>
2018-03-13 02:11:26 +0100

    Messing with set_num_treads()
    Not working, we only have 1 thread running

--- P1/zeta2/zeta2.cpp
+++ P1/zeta2/zeta2.cpp
@@ -6,12 +6,15 @@
 double zeta(int n, int threads){ 
     double S = 0.0;
     int tid;
-    printf("Start running in paralell\n");
-    #pragma omp paralell for num_threads(threads) reduction(+:S) schedule(static)
+    omp_set_dynamic(0);
+    omp_set_num_threads(2);
+    printf(" max threads %d\n",omp_get_max_threads());
+    #pragma omp paralell for reduction(+:S) schedule(static)
     for(int i = 1; i <= n; i++){//each process will do an equal amout of itterations.
         tid = omp_get_thread_num();
+        int num = omp_get_num_threads();
         S +=1.0 /pow(i,2);
-        printf(" thread %d working on %d \n",tid, i);
+        printf(" thread %d of %d working on %d \n",tid+1, num, i);
     }
     return S;
 }
