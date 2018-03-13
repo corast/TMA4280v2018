7b9affa8a0ba91b70ffdf3cd3a95f5b6fbce3195
corast <corastweb94@hotmail.com>
2018-03-13 12:29:13 +0100

    cleaning up

--- P1/zeta2/zeta2.cpp
+++ P1/zeta2/zeta2.cpp
@@ -10,14 +10,12 @@ double zeta(int n, int threads){
     //omp_set_dynamic(0);
     //omp_set_num_threads(8);
     printf(" max threads %d\n",omp_get_max_threads());
-    #pragma omp paralell for num_threads(8) schedule(static)
-    {
+    #pragma omp paralell for num_threads(4) schedule(static)
     for(int i = 1; i <= n; i++){//each process will do an equal amout of itterations.
         tid = omp_get_thread_num();
         int num = omp_get_num_threads();
         S +=1.0 /pow(i,2);
         printf(" thread %d of %d working on %d \n",tid+1, num, i);
-    }
     }
 
     return S;
