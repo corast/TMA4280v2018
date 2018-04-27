f04d68c21cdd407f5196f83500ad9ce3f173f88c
corast <corastweb94@hotmail.com>
2018-04-27 13:25:44 +0200

    fix output

--- P2/loopScript_question3.sh
+++ P2/loopScript_question3.sh
@@ -2,7 +2,11 @@
 arrayp=(2 3 4 6 9 12 18)
 arrayt=(18 12 9 6 4 3 2)
 len=${#arrayt[@]}
+n=$((2**14))
 for ((i = 0; i<${len}; i++));
 do
     echo ${arrayp[$i]} ${arrayt[$i]}
 done
+
+echo $n
+echo $((2**14))
